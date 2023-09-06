c59d3516ab2557225bccd52119f50daff3770476
SthabisoMbongwa <105135056+SthabisoMbongwa@users.noreply.github.com>
2023-09-06 16:02:37 +0200

    Add files via upload

---
+++ My Project/app.js
@@ -0,0 +1,44 @@
+// Database reference: https://online-profile-e324b-default-rtdb.firebaseio.com/
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+
+    //Toggle Nav 
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+          //Animate Links
+    navLinks.forEach((link, index) => {
+        if(link.computedStyleMap.animation){
+            link.computedStyleMap.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+        }
+        });
+
+    //Burger Animation
+        burger.classList.toggle('toggle'); 
+    });
+  
+
+}
+
+navSlide(); 
+
+
+
+var navLinks = document.getElementsByClassName("nav-links");
+var links = navLinks.getElementsByClassName("link");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    var current = navLinks.getElementsByClassName("active");
+  });
+}
+
+
+
+
