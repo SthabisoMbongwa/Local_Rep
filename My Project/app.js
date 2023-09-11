4231cd96a8174686635488f507b57e37e632a95d
Sthabiso <Sthabiso@aims.ac.za>
2023-09-11 15:19:24 +0200

    my last commit

--- My Project/app.js
+++ My Project/app.js
@@ -1,8 +1,144 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Loading
+
+
+
+// Get references to error message elements
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+// Function to validate the Name field
+function validateName() {
+    var name = document.getElementById('contact-name').value;
+    if (name.length == 0) {
+        nameError.innerHTML = 'Full Name is required';
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
+        nameError.innerHTML = 'Please write Full Name';
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+// Function to validate the Email field
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if (!email.match(/^[A-Za-z._-]+@[A-Za-z]+\.[a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Invalid Email Format';
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+// Function to validate the Message field
+function validateMessage() {
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+}
+
+// Function to validate the entire form
+function validateForm() {
+    if (!validateName() || !validateEmail() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix errors to submit';
+        setTimeout(function () {
+            submitError.style.display = 'none';
+        }, 3000);
+        return false;
+    }
+}
+
+// Add an event listener to the form to call validateForm() when submitted
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        if (!validateForm()) {
+            event.preventDefault();
+        }
+    });
+});
+
+
+
+
+
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    //Toggle Nav 
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.computedStyleMap.animation) {
+                link.computedStyleMap.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+
+}
+
+navSlide();
+
+
+
+// Get all elements with class "nav-links"
+var navLinks = document.getElementsByClassName("nav-links");
+
+// Iterate through each ".nav-links" element
+for (var i = 0; i < navLinks.length; i++) {
+    // Get all elements with class "link" within the current ".nav-links" element
+    var links = navLinks[i].getElementsByClassName("link");
+
+    // Iterate through each link within the current ".nav-links" element
+    for (var j = 0; j < links.length; j++) {
+        // Add a click event listener to the link
+        links[j].addEventListener("click", function () {
+            // Within this function, "this" refers to the clicked link element
+            // You can access its attributes or perform actions here
+            // Example: To add a class "active" to the clicked link
+            this.classList.add("active");
+
+            // Your logic here
+        });
+    }
+}
+
+
+
 const textureLoader = new THREE.TextureLoader()
 const normalTexture = textureLoader.load('/texture/height3.jpg')
 
@@ -138,51 +274,5 @@ const tick = () => {
 }
 
 tick()
-
-
-
-
-
-
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
-
-    //Toggle Nav 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-          //Animate Links
-    navLinks.forEach((link, index) => {
-        if(link.computedStyleMap.animation){
-            link.computedStyleMap.animation = ''
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
-        }
-        });
-
-    //Burger Animation
-        burger.classList.toggle('toggle'); 
-    });
-  
-
-}
-
-navSlide(); 
-
-
-
-var navLinks = document.getElementsByClassName("nav-links");
-var links = navLinks.getElementsByClassName("link");
-
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-    var current = navLinks.getElementsByClassName("active");
-  });
-}
-
-
 
 
