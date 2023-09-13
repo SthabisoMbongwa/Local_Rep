f69c83069cf982e1371d57e31f928f53700322af
Sthabiso <Sthabiso@aims.ac.za>
2023-09-13 17:42:05 +0200

    last commit

--- main.js
+++ main.js
@@ -1,78 +1,78 @@
 
 // Get references to error message elements
-var nameError = document.getElementById('name-error');
-var emailError = document.getElementById('email-error');
-var messageError = document.getElementById('message-error');
-var submitError = document.getElementById('submit-error');
+// var nameError = document.getElementById('name-error');
+// var emailError = document.getElementById('email-error');
+// var messageError = document.getElementById('message-error');
+// var submitError = document.getElementById('submit-error');
 
-// Function to validate the Name field
-function validateName() {
-    var name = document.getElementById('contact-name').value;
-    if (name.length == 0) {
-        nameError.innerHTML = 'Full Name is required';
-        return false;
-    }
-    if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
-        nameError.innerHTML = 'Please write Full Name';
-        return false;
-    }
+// // Function to validate the Name field
+// function validateName() {
+//     var name = document.getElementById('contact-name').value;
+//     if (name.length == 0) {
+//         nameError.innerHTML = 'Full Name is required';
+//         return false;
+//     }
+//     if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
+//         nameError.innerHTML = 'Please write Full Name';
+//         return false;
+//     }
 
-    nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
-    return true;
-}
+//     nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+//     return true;
+// }
 
-// Function to validate the Email field
-function validateEmail() {
-    var email = document.getElementById('contact-email').value;
+// // Function to validate the Email field
+// function validateEmail() {
+//     var email = document.getElementById('contact-email').value;
 
-    if (email.length == 0) {
-        emailError.innerHTML = 'Email is required';
-        return false;
-    }
-    if (!email.match(/^[A-Za-z._-]+@[A-Za-z]+\.[a-z]{2,4}$/)) {
-        emailError.innerHTML = 'Invalid Email Format';
-        return false;
-    }
+//     if (email.length == 0) {
+//         emailError.innerHTML = 'Email is required';
+//         return false;
+//     }
+//     if (!email.match(/^[A-Za-z._-]+@[A-Za-z]+\.[a-z]{2,4}$/)) {
+//         emailError.innerHTML = 'Invalid Email Format';
+//         return false;
+//     }
 
-    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
-    return true;
-}
+//     emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+//     return true;
+// }
 
-// Function to validate the Message field
-function validateMessage() {
-    var message = document.getElementById('contact-message').value;
-    var required = 30;
-    var left = required - message.length;
+// // Function to validate the Message field
+// function validateMessage() {
+//     var message = document.getElementById('contact-message').value;
+//     var required = 30;
+//     var left = required - message.length;
 
-    if (left > 0) {
-        messageError.innerHTML = left + ' more characters required';
-        return false;
-    }
+//     if (left > 0) {
+//         messageError.innerHTML = left + ' more characters required';
+//         return false;
+//     }
 
-    messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
-    return true;
-}
+//     messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
+//     return true;
+// }
 
-// Function to validate the entire form
-function validateForm() {
-    if (!validateName() || !validateEmail() || !validateMessage()) {
-        submitError.style.display = 'block';
-        submitError.innerHTML = 'Please fix errors to submit';
-        setTimeout(function () {
-            submitError.style.display = 'none';
-        }, 3000);
-        return false;
-    }
-}
+// // Function to validate the entire form
+// function validateForm() {
+//     if (!validateName() || !validateEmail() || !validateMessage()) {
+//         submitError.style.display = 'block';
+//         submitError.innerHTML = 'Please fix errors to submit';
+//         setTimeout(function () {
+//             submitError.style.display = 'none';
+//         }, 3000);
+//         return false;
+//     }
+// }
 
-// Add an event listener to the form to call validateForm() when submitted
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        if (!validateForm()) {
-            event.preventDefault();
-        }
-    });
-});
+// // Add an event listener to the form to call validateForm() when submitted
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('contact-form').addEventListener('submit', function (event) {
+//         if (!validateForm()) {
+//             event.preventDefault();
+//         }
+//     });
+// });
 
 
 
