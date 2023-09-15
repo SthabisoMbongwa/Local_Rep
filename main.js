acd306f857ed68157e7bfa3db5b0347a4d28c2c5
Sthabiso <Sthabiso@aims.ac.za>
2023-09-15 14:43:59 +0200

    successfully added mailJs

--- main.js
+++ main.js
@@ -1,79 +1,30 @@
 
-// Get references to error message elements
-// var nameError = document.getElementById('name-error');
-// var emailError = document.getElementById('email-error');
-// var messageError = document.getElementById('message-error');
-// var submitError = document.getElementById('submit-error');
+function sendMail() {
+    event.preventDefault();
+    console.log("Function called");
+    var params = {
+        name: document.getElementById("contact-name").value,
+        email: document.getElementById("contact-email").value,
+        subject: document.getElementById("contact-subject").value,
+        message: document.getElementById("contact-message").value
+    };
+    
+    const serviceID = "service_oqmrksj";
+    const templateID = "template_0iomx5h";
 
-// // Function to validate the Name field
-// function validateName() {
-//     var name = document.getElementById('contact-name').value;
-//     if (name.length == 0) {
-//         nameError.innerHTML = 'Full Name is required';
-//         return false;
-//     }
-//     if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
-//         nameError.innerHTML = 'Please write Full Name';
-//         return false;
-//     }
-
-//     nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
-//     return true;
-// }
-
-// // Function to validate the Email field
-// function validateEmail() {
-//     var email = document.getElementById('contact-email').value;
-
-//     if (email.length == 0) {
-//         emailError.innerHTML = 'Email is required';
-//         return false;
-//     }
-//     if (!email.match(/^[A-Za-z._-]+@[A-Za-z]+\.[a-z]{2,4}$/)) {
-//         emailError.innerHTML = 'Invalid Email Format';
-//         return false;
-//     }
-
-//     emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
-//     return true;
-// }
-
-// // Function to validate the Message field
-// function validateMessage() {
-//     var message = document.getElementById('contact-message').value;
-//     var required = 30;
-//     var left = required - message.length;
-
-//     if (left > 0) {
-//         messageError.innerHTML = left + ' more characters required';
-//         return false;
-//     }
-
-//     messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
-//     return true;
-// }
-
-// // Function to validate the entire form
-// function validateForm() {
-//     if (!validateName() || !validateEmail() || !validateMessage()) {
-//         submitError.style.display = 'block';
-//         submitError.innerHTML = 'Please fix errors to submit';
-//         setTimeout(function () {
-//             submitError.style.display = 'none';
-//         }, 3000);
-//         return false;
-//     }
-// }
-
-// // Add an event listener to the form to call validateForm() when submitted
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.getElementById('contact-form').addEventListener('submit', function (event) {
-//         if (!validateForm()) {
-//             event.preventDefault();
-//         }
-//     });
-// });
-
+    emailjs.send(serviceID, templateID, params)
+    .then(
+        function(response) {
+            console.log("Email sent successfully:", response);
+            alert("Your message has been sent successfully!");
+            document.getElementById("contact-form").reset(); // Reset the form
+        },
+        function(error) {
+            console.error("Email sending failed:", error);
+            alert("Oops! Something went wrong. Please try again later.");
+        }
+    );
+}
 
 
 
